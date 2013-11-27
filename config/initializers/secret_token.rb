2172674c57bcf3f573dba3c2a7c3b89fb70d0b64
Linda Marsh <linda@pathosien.com>
2013-11-27 15:07:56 -0800

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PathosienTest::Application.config.secret_key_base = 'b906555b5e58f2c655e0103d4409c37d43e7e5aadde5a3e0c19a8d9b27d77095ece4dd8e561f95199a5ca95f484ad5626d45d061a40d84b0acbc2ff5f1954aff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PathosienTest::Application.config.session_store :cookie_store, key: '_PathosienTest_session'
